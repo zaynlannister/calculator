0f9c05505c8a86f4484176239808861067342ca5
zaynlannister <zaynlannister@gmail.com>
2022-08-14 23:29:17 +0500

    add products

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
-import '../scss/app.scss'
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
+import '../scss/app.scss'
+import './script'
---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const products = [
+    {
+        name: 'brick',
+        price: 49.3,
+    },
+
+    {
+        name: 'concrete',
+        price: 79.6,
+    },
+
+    {
+        name: 'tile',
+        price: 53.4,
+    },
+
+    {
+        name: 'stone',
+        price: 105,
+    }
+]
