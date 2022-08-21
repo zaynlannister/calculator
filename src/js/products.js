9216fc3acf57ba2479bd5941c16f24fafdcf9ddc
zaynlannister <zaynlannister@gmail.com>
2022-08-21 13:37:18 +0500

    remove products to another js file & import it

---
+++ src/js/products.js
@@ -0,0 +1,25 @@
+export const products = [
+    {
+        name: 'brick',
+        priceForSize: 49.3,
+        priceForWeight: 4.4
+    },
+
+    {
+        name: 'concrete',
+        priceForSize: 79.6,
+        priceForWeight: 17.3
+    },
+
+    {
+        name: 'tile',
+        priceForSize: 53.4,
+        priceForWeight: 4.2
+    },
+
+    {
+        name: 'stone',
+        priceForSize: 105,
+        priceForWeight: 21.8
+    }
+]
